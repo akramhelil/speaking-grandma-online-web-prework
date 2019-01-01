cbb7c6fa40d769647681a0aa94f4e35fb22b982f
Akram Helil <akramhelil@gmail.com>
2019-01-01 18:47:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,7 @@ def speak_to_grandma(phrase)
 else
   return "NO, NOT SINCE 1938!"
 elsif condition (speak_to_grandma == "I LOVE YOU GRANDMA!")
-  return "I LOVE YOU TOO PUMPKIN!"
-  
+  return "I LOVE YOU TOO PUMPKIN!"  
   end
 end
 speak_to_grandma == "I LOVE YOU GRANDMA!"
